d1ab319be55f712df4dcc9dc35118cecec115c0b
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-31 14:29:26 -0500

    Example one: using the Function interface to define first-class functions and function chaining.

---
+++ src/functionalInterfaces/ExerciseTwo/User.java
@@ -0,0 +1,63 @@
+package functionalInterfaces.ExerciseTwo;
+
+/**
+ *      Domain class for User Object
+ * */
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+
+    public User(UUID userId, String firstName, String lastName) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+
+    @Override
+    public String toString() {
+        return "User { userId = %s , name = '%s', lastName = '%s'}".formatted(userId, firstName, lastName);
+    }
+}
