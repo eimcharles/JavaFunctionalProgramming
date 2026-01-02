78057cf7988771737155fd7eceddd3642dbf9408
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-01-02 15:23:41 -0500

    Example three: using the BiConsumer interface to define functional identities for relational logic, state mutation, and definition-time chaining.

---
+++ src/domain/Booking.java
@@ -0,0 +1,71 @@
+package domain;
+
+/**
+ *      Domain class for Booking Object
+ * */
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Booking {
+
+    private final UUID userBookingID;
+    private final User user;
+    private final Car car;
+    private final LocalDateTime bookingTime;
+    private boolean isBookingActive;
+
+    public Booking(UUID userBookingID, User user, Car car, LocalDateTime bookingTime) {
+        this.userBookingID = userBookingID;
+        this.user = user;
+        this.car = car;
+        this.bookingTime = bookingTime;
+        this.isBookingActive = true;
+    }
+
+    public UUID getUserBookingID() {
+        return userBookingID;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public boolean isBookingActive() {
+        return isBookingActive;
+    }
+
+    public void cancelBooking() {
+        this.isBookingActive = false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+
+        // Compares the unique, immutable ID
+        return Objects.equals(userBookingID, booking.userBookingID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userBookingID);
+    }
+
+    @Override
+    public String toString() {
+        return "Booking { userBookingID = %s, user = %s, car = %s, bookingTime = %s, isBookingActive = %s }".formatted(userBookingID, user, car, bookingTime, isBookingActive);
+    }
+}
