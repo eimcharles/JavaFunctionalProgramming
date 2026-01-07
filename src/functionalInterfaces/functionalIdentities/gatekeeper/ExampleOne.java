085264e504a3a3c6461ad343f42f82f3491dc65a
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-01-07 13:48:09 -0500

    Gatekeeper example: adding comments, adjusting examples and adding an example for conditional execution.

--- src/functionalInterfaces/functionalIdentities/gatekeeper/ExampleOne.java
+++ src/functionalInterfaces/functionalIdentities/gatekeeper/ExampleOne.java
@@ -5,6 +5,7 @@ import domain.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class ExampleOne {
@@ -18,15 +19,13 @@ public class ExampleOne {
          *      a single input argument and returns a primitive boolean.
          *
          *      Predicate is the "Gatekeeper" of functional pipelines:
-         *
-         *      - Filter Logic: Deciding what data stays or goes
+         *      - Filter Logic (Streams): Deciding what data stays or goes
          *      - Validation: Checking if an object meets requirements
          *      - Conditional Execution: Deciding if an action should trigger
          *
          *      T (Input): The type of the argument to be tested.
          *
          *      Predicate methods are pure functions:
-         *
          *      - No State Mutation
          *      - Deterministic
          *
@@ -42,48 +41,61 @@ public class ExampleOne {
 
         Booking charlesBooking =  new Booking(UUID.randomUUID(), charles, mercedes, LocalDateTime.now());
 
-        ///  Example 1:
+        ///  Example 1: Validation
         System.out.println("Example one: using Predicate < T > interface to test a condition");
 
         if (isElectric.test(mercedes))
-            System.out.println(mercedes.getBrand() + " is electric " + mercedes.isElectric());
+            System.out.println(mercedes.getBrand() + " is electric ");
         System.out.println();
 
-        ///  Example 2:
+        ///  Example 2: Validation
         System.out.println("Example two: using Predicate < T > interface to test a condition");
 
-        if (isCarAvailable.test(mercedes))
-            System.out.println(mercedes.getBrand() + " is booked " + mercedes.isCarBooked());
+        if (isCarBooked.test(mercedes))
+            System.out.println(mercedes.getBrand() + " is already booked");
         else {
-            System.out.println(mercedes.getBrand() + " is booked " + mercedes.isCarBooked());
+            System.out.println(mercedes.getBrand() + " is not booked and available");
         }
         System.out.println();
 
-        ///  Example 3:
+        ///  Example 3: Definition-Time Chaining
         System.out.println("Example three: using Predicate < T > interface to test two conditions using definition time chaining");
 
-        if (IsElectricAndCarAvailable.test(mercedes))
-            System.out.println(mercedes.getBrand() + " is electric " + mercedes.isElectric() + " and booked " + mercedes.isCarBooked());
+        if (isElectricAndIsCarBooked.test(mercedes))
+            System.out.println(mercedes.getBrand() + " is electric and is already booked");
         else {
-            System.out.println(mercedes.getBrand() + " is electric " + mercedes.isElectric() + " and booked " + mercedes.isCarBooked());
+            System.out.println(mercedes.getBrand() + " is electric and is not booked and available");
         }
         System.out.println();
 
-        ///  Example 4:
-        System.out.println("Example four: using Predicate < T > interface to test a condition");
+        ///  Example 4: Conditional execution
+        System.out.println("Example four: using Predicate < T > for conditional execution");
 
         if (isBookingActive.test(charlesBooking))
-            System.out.println("Booking for " + charlesBooking.getUser().getFirstName() + " is active " + charlesBooking.isBookingActive());
+            releaseCarFromBookingAndRefundUser.accept(mercedes, charlesBooking);
         System.out.println();
 
     }
 
     static Predicate<Car> isElectric = car -> car.getFuelType() == FuelType.ELECTRIC;
 
-    static Predicate<Car> isCarAvailable = Car::isCarBooked;
+    static Predicate<Car> isCarBooked = Car::isCarBooked;
 
-    static Predicate<Car> IsElectricAndCarAvailable = isElectric.and(isCarAvailable);
+    static Predicate<Car> isElectricAndIsCarBooked = isElectric.and(isCarBooked);
 
     static Predicate<Booking> isBookingActive = Booking::isBookingActive;
+
+    static BiConsumer<Car, Booking> releaseCarFromBooking =  (car, booking) -> {
+        car.setCarBooked(false);
+        booking.cancelBooking();
+        System.out.println("Booking number %s cancelled".formatted(booking.getUserBookingID()));
+    };
+
+    static BiConsumer<Car, Booking> refundUser = (car, booking) -> {
+        System.out.println("Processing refund for booking: " + booking.getUserBookingID()
+                + " for car with registration number " + car.getRegistrationNumber());
+    };
+
+    static BiConsumer<Car, Booking> releaseCarFromBookingAndRefundUser = releaseCarFromBooking.andThen(refundUser);
 
 }
