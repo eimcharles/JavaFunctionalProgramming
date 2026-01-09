f6a8e1db1f7f9aa670481def6212a16671eafe22
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-01-09 13:22:19 -0500

    Example two: implementing callback functions in Java using functional interfaces for inversion of control.

---
+++ src/functionalInterfaces/javaCallbackFunctions/ExampleTwo.java
@@ -0,0 +1,78 @@
+package functionalInterfaces.javaCallbackFunctions;
+
+import domain.User;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class ExampleTwo {
+
+    /**
+     *      Inversion of control:
+     *
+     *      The Main method acts like a container
+     *      that injects specific validation
+     *      and behavioral dependencies
+     *      into the helloUser method.
+     *
+     *      The Main method defines
+     *      the "how" and "what" and
+     *      helloUser defines the
+     *      "when".
+     *
+     *      Ioc promotes loose coupling
+     *      for dependencies.
+     *
+     *      Traditional Programming:
+     *
+     *      The helloUser method act like a "Silo"
+     *      where behavior is hardcoded
+     *      internally.
+     *
+     *      The helloUser defines
+     *      the "how" and "when"
+     *      and "what".
+     *
+     *      This results in tight coupling,
+     *      changes to "how", "when" and
+     *      "what" require modifying the
+     *      method itself.
+     *
+     *      This violates the Open-Closed Principle,
+     *      as the method is not open for extension
+     *      without modifying its source code.
+     */
+
+    public static void main(String[] args) {
+
+        User invalidUser = new User(UUID.randomUUID(), "Jerry", null);
+
+        helloUser(invalidUser,
+
+                ///  Loose coupling - Validation Dependency
+                user -> user.getLastName() != null,
+
+                ///  Loose coupling - Strategy Dependency
+                user -> System.out.println(user.getFirstName() + " you must have a last name")
+
+                );
+
+    }
+
+    static void helloUser(User user, Predicate<User> validator, Consumer<User> callback) {
+        if (validator.test(user)) {
+            System.out.println("Hello " + user.getFirstName() + " " + user.getLastName());
+        } else {
+            callback.accept(user);
+        }
+    }
+
+    static void helloUser(User user) {
+        if (user.getLastName() != null) {
+            System.out.println("Hello " + user.getFirstName() + " " + user.getLastName());
+        } else {
+            System.out.println(user.getFirstName() + " you must have a last name");
+        }
+    }
+}
