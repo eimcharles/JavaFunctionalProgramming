4a55259536cc19ec299be8662e6a29755b4e16fb
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-01-14 13:58:29 -0500

    Booking example: filtering active bookings, mapping associated cars, and identifying available inventory using list comparison.

---
+++ src/main/java/com/eimc/streams/transformations/BookingExample.java
@@ -0,0 +1,61 @@
+package com.eimc.streams.transformations;
+
+import com.eimc.domain.Booking;
+import com.eimc.domain.Car;
+import com.eimc.streams.mockData.MockData;
+
+import java.util.List;
+
+public class BookingExample {
+
+    public static void main(String[] args) {
+
+        List<Booking> bookingList = MockData.getBookings();
+        List<Car> carList = MockData.getCars();
+
+        /// Example 1:
+        System.out.println("Example one: display all bookings from the bookingList");
+        bookingList.forEach(System.out::println);
+        System.out.println();
+
+        ///  Example 2:
+        System.out.println("Example two: display all active booking from the bookingList");
+        List<Booking> activeBookings = bookingList
+                .stream()
+                .filter(Booking::isBookingActive)
+                .toList();
+
+        activeBookings.forEach(System.out::println);
+        System.out.println();
+
+        ///  Example 3:
+        System.out.println("Example three: display all active bookings and associated booked cars from the bookingList");
+        List<Car> bookedCars = activeBookings.stream()
+                ///  Takes a booking a returns the associated car object
+                .map(booking -> booking.getCar())
+                .toList();
+
+        bookedCars.forEach(System.out::println);
+        System.out.println();
+
+        ///  Example 4: return the available cars based on the booked cars list
+        System.out.println("Example four: display all available cars that are not associated to a booking from the bookingList");
+        List<Car> availableCars = carList.stream()
+                ///  Filter the available cars based on booked cars
+                .filter(car -> !bookedCars.contains(car))
+                .toList();
+
+        availableCars.forEach(System.out::println);
+        System.out.println();
+
+        ///  Example 5: look at the car list and get all available cars
+        System.out.println("Example five: display all available cars in carList using the booked cars in the bookingList ");
+        List<Car> availableCarsInCarList = carList.stream()
+                .filter(car -> !car.isCarBooked())
+                .toList();
+
+        availableCarsInCarList.forEach(System.out::println);
+        System.out.println();
+
+    }
+}
